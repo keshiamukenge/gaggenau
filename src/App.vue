d367f34592929b1e4083124323ce384ebd8bd05b
Keshia Mukenge <keshia@MBP-de-Keshia-002.lan>
2023-01-29 20:09:53 +0100

    fix: img webgl loader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
+  <div v-if="start">
     <Header />
     <RouterView />
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { RouterView } from "vue-router";
 
 import Header from "@/components/Header/Header.vue";
 import "./assets/scss/main.scss";
+
+const start = ref(false);
+
+setTimeout(() => {
+  start.value = true;
+}, 2000);
 </script>
