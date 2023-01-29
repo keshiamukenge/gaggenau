b3bdedb3a374e493550a437378c3bb115439cae9
Keshia Mukenge <keshia@MBP-de-Keshia-002.lan>
2023-01-29 20:09:53 +0100

    fix: img webgl loader

--- src/components/Webgl/Webgl.vue
+++ src/components/Webgl/Webgl.vue
@@ -42,47 +42,49 @@ function setMousePosition() {
 const webglStore = useWebglStore();
 
 onMounted(() => {
-  const canvas = document.querySelector("canvas");
-  canvas.setAttribute("id", "canvas");
+  setTimeout(() => {
+    const canvas = document.querySelector("canvas");
+    canvas.setAttribute("id", "canvas");
 
-  const time = new THREE.Clock();
-  const meshes = scene.value.scene.children;
+    const time = new THREE.Clock();
+    const meshes = scene.value.scene.children;
 
-  setMousePosition();
+    setMousePosition();
 
-  renderer.value.onAfterRender(() => {
-    //set raycaster
-    renderer.value.three.pointer.intersect();
-  });
-
-  renderer.value.onBeforeRender(() => {
-    // set time
-    const elapsedTime = time.getElapsedTime();
-
-    webglStore.planes.value.forEach((plane, id) => {
-      // set scale
-      meshes[id].scale.set(
-        plane.imageElement.el.getBoundingClientRect().width,
-        plane.imageElement.el.getBoundingClientRect().height,
-        1
-      );
-
-      // set position
-      meshes[id].position.x =
-        plane.imageElement.el.getBoundingClientRect().x -
-        window.innerWidth / 2 +
-        plane.imageElement.el.getBoundingClientRect().width / 2;
-
-      meshes[id].position.y =
-        -plane.imageElement.el.getBoundingClientRect().y +
-        window.innerHeight / 2 -
-        plane.imageElement.el.getBoundingClientRect().height / 2;
-
-      meshes[id].position.z = 1;
-
-      // set uniforms
-      plane.mesh.material.uniforms.uTime.value = elapsedTime * 5;
+    renderer.value.onAfterRender(() => {
+      //set raycaster
+      renderer.value.three.pointer.intersect();
     });
-  });
+
+    renderer.value.onBeforeRender(() => {
+      // set time
+      const elapsedTime = time.getElapsedTime();
+
+      webglStore.planes.value.forEach((plane, id) => {
+        // set scale
+        meshes[id].scale.set(
+          plane.imageElement.el.getBoundingClientRect().width,
+          plane.imageElement.el.getBoundingClientRect().height,
+          1
+        );
+
+        // set position
+        meshes[id].position.x =
+          plane.imageElement.el.getBoundingClientRect().x -
+          window.innerWidth / 2 +
+          plane.imageElement.el.getBoundingClientRect().width / 2;
+
+        meshes[id].position.y =
+          -plane.imageElement.el.getBoundingClientRect().y +
+          window.innerHeight / 2 -
+          plane.imageElement.el.getBoundingClientRect().height / 2;
+
+        meshes[id].position.z = 1;
+
+        // set uniforms
+        plane.mesh.material.uniforms.uTime.value = elapsedTime * 5;
+      });
+    });
+  }, 2000);
 });
 </script>
